ce87da342b7758b8bff5b75498a6595fd6bd5738
Taruni Sanjay <tsanjay@ucompute1.physics.umd.edu>
2022-11-15 02:44:17 -0500

    Added input fifos instantiation list and display message to test enable

--- test/test_verilog/test_enable/test01/testbench.v
+++ test/test_verilog/test_enable/test01/testbench.v
@@ -10,30 +10,44 @@ module testbench();
    reg [log2(buffer_size) - 1 : 0] data_pop;
    wire [log2(buffer_size) - 1 : 0] result_free_space;
    wire [log2(buffer_size) - 1 : 0] status_free_space;
+   reg rd_en_fifo_data;
    reg [2 : 0] mode;//3-bit operations mode
+   reg [2 : 0] next_mode;
    reg [4 : 0] b;
    reg [3 : 0] N;
 /*   reg [word_size - 1 : 0] command_in;
    reg [word_size - 1 : 0] data_in;*/
 // Outputs to enable module
    wire enable;
-   wire 
 // Inputs to FIFO modules
+   reg wr_en_input;
    reg result_wr_en, result_rd_en;
    reg [word_size - 1 : 0] result_data_in;
-   
+   wire [log2(buffer_size - 1 : 0] free_space_fifo_data, free_space_fifo_command;
+   wire [log2(buffer_size - 1 : 0] free_space_out_fifo_result, free_space_out_fifo_status;
+   wire [word_size - 1 : 0] data_in_fifo_data, data_in_fifo_command;
+   wire [2*word_size - 1 : 0] data_out_fifo_result, data_out_fifo_status;
    
    integer i;
    
 /******************************************************
  Instantiate 4 FIFOs and the enable module for the PEA
  *******************************************************/
+/*Haven't included rd_en_input signals yet in the port list*/
+  fifo #(word_size, buffer_size) fifo_in_data(clk, rst, wr_en_input, data_pop, data_in, free_space_fifo_data, data_in_fifo_data);
 
-   // Instantiate the enable module
+  fifo #(word_size, buffer_size) fifo_in_command(clk, rst, wr_en_input, command_pop, command_in, free_space_fifo_command, data_in_fifo_command);
+ 
+  fifo #(2*word_size, buffer_size) fifo_out_result(clk, rst, rd_en_fifo_data, free_space_out_fifo_result, data_out_fifo_result);
+ 
+  fifo #(2*word_size, buffer_size) fifo_out_status(clk, rst, rd_en_fifo_data, free_space_out_fifo_status, data_out_fifo_status);
+  
+/*****************************************************
+Instantiation of enable module
+*****************************************************/
    PEA_enable #(word_size, buffer_size) enable(rst, control_pop, data_pop, result_free_space, status_free_space, mode, control_in, enable);
 
-   // Instantiate the 4 FIFOs: control, data, result, and status
-   
+  integer descr; 
    
    
 initial begin
@@ -44,6 +58,21 @@ initial begin
    end
 
 end
+
+initial 
+begin
+    #1;
+    rst <= 0;
+    wr_en_input <= 0;
+    data_pop <= 0;
+    command_pop <= 0;
+    next_mode <= GET_COMMAND;
+    rd_en_fifo_data <= 0;
+    #2 rst <= 1;
+    #2;
+
+    $fdisplay(descr, "Setting input fifos");
+   
    
 
 
