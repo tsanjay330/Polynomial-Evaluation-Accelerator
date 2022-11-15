5282139075bb6c55f7e93554d1cbe0309258df9a
Taruni Sanjay <tsanjay@ucompute1.physics.umd.edu>
2022-11-15 01:34:28 -0500

    Made changes to the test_enable subdir

--- test/test_verilog/test_enable/test01/testbench.v
+++ test/test_verilog/test_enable/test01/testbench.v
@@ -1,27 +1,29 @@
-
 `timescale 1ns / 1ps
 module testbench();
-   localparam buffer_size = 1024;
-   localparam word_size = 16;
+    parameter word_size = 16, buffer_size = 1024;
+    parameter GET_COMMAND = 3'b000, STP = 3'b001, EVP = 3'b010, EVB = 3'b011, OUTPUT = 3'b100, RST = 3'b101;
 
 // Inputs to enable module (all wires here are outputs from FIFO modules)
    reg clk;
    reg rst;
-   reg [log2(buffer_size) - 1 : 0] control_pop;
+   reg [log2(buffer_size) - 1 : 0] command_pop;
    reg [log2(buffer_size) - 1 : 0] data_pop;
    wire [log2(buffer_size) - 1 : 0] result_free_space;
-   reg [log2(buffer_size) - 1 : 0] status_free_space;
-   reg [1 : 0] 			   mode;
-   reg [word_size - 1 : 0] 	   control_in;
+   wire [log2(buffer_size) - 1 : 0] status_free_space;
+   reg [2 : 0] mode;//3-bit operations mode
+   reg [4 : 0] b;
+   reg [3 : 0] N;
+/*   reg [word_size - 1 : 0] command_in;
+   reg [word_size - 1 : 0] data_in;*/
 // Outputs to enable module
-   wire 			   enable;
-
+   wire enable;
+   wire 
 // Inputs to FIFO modules
-   reg 				   result_wr_en, result_rd_en;
-   reg [word_size - 1 : 0] 	   result_data_in;
+   reg result_wr_en, result_rd_en;
+   reg [word_size - 1 : 0] result_data_in;
    
    
-   integer 			   i;
+   integer i;
    
 /******************************************************
  Instantiate 4 FIFOs and the enable module for the PEA
