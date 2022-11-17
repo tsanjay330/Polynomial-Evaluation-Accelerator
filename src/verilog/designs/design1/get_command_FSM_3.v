45e530876b68645a02502df7a132516aae2a2a75
Erin Quartararo <erinq20@ucompute2.physics.umd.edu>
2022-11-17 16:15:29 -0500

    Minor changes to get_command_FSM_3

--- src/verilog/designs/design1/get_command_FSM_3.v
+++ src/verilog/designs/design1/get_command_FSM_3.v
@@ -4,17 +4,17 @@ module get_command_FSM_3
         (
         input clk, rst,
         input start_get_cmd,
-		input [15 : 0] commandstr,
+		input [15 : 0] command_in,
 		input en_proc_cmd,
+		input [3 : 0] N [7 : 0],
 		output reg en_rd_cmd,
         output reg done_get_cmd,
-        output reg [7 : 0] command,
+        output reg [7 : 0] instr,
         output reg [2 : 0] arg1,
 		output reg [4 : 0] arg2,
-		output reg [1 : 0] error); //TODO: correct bit width of error
+		output reg [1 : 0] error);
 
-		reg [15 : 0] commandstr;
-		reg [7 : 0] next_command;
+		reg [7 : 0] next_instr;
 		reg [2 : 0] next_arg1;
 		reg [4 : 0] next_arg2;
 
@@ -23,13 +23,13 @@ module get_command_FSM_3
 	always @(posedge clk or negedge reset)
 		if (! rst)
 			state <= STATE_START;
-			command <= 0;
+			instr <= 0;
 			arg1 <= 0;
 			arg2 <= 0;
 			error <= 0;
 		else
 			state <= next_state;
-			command <= next_command;
+			instr <= next_instr;
 			arg1 <= next_arg1;
 			arg2 <= next_arg2;
 			error <= next_error;
@@ -57,12 +57,12 @@ module get_command_FSM_3
 		endcase
 	end
 
-	always @(state, start_get_cmd, commandstr, command, arg1, arg2, error) begin
+	always @(state, start_get_cmd, command_in, instr, arg1, arg2, error) begin
 		case (state)
 			STATE_START:
 				done_get_cmd <= 0;
 				en_rd_cmd <= 0;
-				next_command <= command;
+				next_instr <= instr;
 				next_arg1 <= arg1;
 				next_arg2 <= arg2;
 				next_error <= error;
@@ -70,51 +70,69 @@ module get_command_FSM_3
 			STATE_GET_CMD:
 				done_get_cmd <= 0;
 				en_rd_cmd <= 1;
-				next_command <= command;
+				next_instr <= instr;
 				next_arg1 <= arg1;
                 next_arg2 <= arg2;
                 next_error <= error;
 
 			STATE_CHECK_ERR:
-				case (commandstr[15 : 8])
+				case (command_in[15 : 8])
 					0: 
-						if (commandstr[4 : 0] <= 10) begin
+						if (command_in[4 : 0] <= 10) begin
 							done_get_cmd <= 0;
 							en_rd_cmd <= 0;
-							next_command <= commandstr[15 : 8];
-							next_arg1 <= commandstr[7 : 5];
-							next_arg2 <= commandstr[4 : 0];
+							next_instr <= command_in[15 : 8];
+							next_arg1 <= command_in[7 : 5];
+							next_arg2 <= command_in[4 : 0];
 							next_error <= 0;
 						end
 			 			else begin
 							done_get_cmd <= 0;
 							en_rd_cmd <= 0;
-							next_command <= command;
+							next_instr <= instr;
 							next_arg1 <= arg1;
 							next_arg2 <= arg2;
 							next_error <= 2;
 						end
 		
 					1:
-						// TODO: add error handling for using S_A before setting S_A
-						done_get_cmd <= 0;
-                        en_rd_cmd <= 0;
-                        next_command <= commandstr[15 : 8];
-                        next_arg1 <= commandstr[7 : 5];
-                        next_arg2 <= 0
-                        next_error <= 0;
+						if (N[command_in[7 : 5]] != 15) begin
+							done_get_cmd <= 0;
+                        	en_rd_cmd <= 0;
+                        	next_instr <= command_in[15 : 8];
+                        	next_arg1 <= command_in[7 : 5];
+                        	next_arg2 <= 0
+                        	next_error <= 0;
+						end
+						else begin
+							done_get_cmd <= 0;
+                            en_rd_cmd <= 0;
+                            next_instr <= instr;
+                            next_arg1 <= arg1;
+                            next_arg2 <= arg2;
+                            next_error <= 3;
+
 					2:
-						done_get_cmd <= 0;
-						en_rd_cmd <= 0;
-                        next_command <= commandstr[15 : 8];
-                        next_arg1 <= commandstr[7 : 5];
-                        next_arg2 <= commandstr[4 : 0];
-                        next_error <= 0;
+						if (N[command_in[7 : 5]] != 15) begin
+                            done_get_cmd <= 0;
+                            en_rd_cmd <= 0;
+                            next_instr <= command_in[15 : 8];
+                            next_arg1 <= command_in[7 : 5];
+                            next_arg2 <= command_in[4 : 0];
+                            next_error <= 0;
+                        end
+                        else begin
+                            done_get_cmd <= 0;
+                            en_rd_cmd <= 0;
+                            next_instr <= instr;
+                            next_arg1 <= arg1;
+                            next_arg2 <= arg2;
+                            next_error <= 3;
 
 					3:
 						done_get_cmd <= 0;
 						en_rd_cmd <= 0;
-						next_command <= commandstr[15 : 8];
+						next_instr <= command_in[15 : 8];
 						next_arg1 <= 0;
 						next_arg2 <= 0;
 						next_error <= 0;
@@ -122,7 +140,7 @@ module get_command_FSM_3
 					default:
 						done_get_cmd <= 0;
 						en_rd_cmd <= 0;
-						next_command <= command;
+						next_instr <= instr;
 						next_arg1 <= arg1;
 						next_arg2 <= arg2;
 						next_error <= 1;
@@ -132,7 +150,7 @@ module get_command_FSM_3
 			STATE_END:
 				done_get_cmd <= 1;
 				en_rd_cmd <= 0;
-				next_command <= command;
+				next_instr <= instr;
 				next_arg1 <= arg1;
 				next_arg2 <= arg2;
 				next_error <= error;
