5c97bc214f3aca14a25cc1be7b3561ba7fa9f135
Erin Quartararo <erinq20@ucompute2.physics.umd.edu>
2022-11-30 20:31:22 -0500

    Debugged get_command and confirmed it compiles

--- src/verilog/designs/design_1/get_command_FSM_3.v
+++ src/verilog/designs/design_1/get_command_FSM_3.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ps
 
 module get_command_FSM_3
-        (
+        #(parameter buffer_size = 1024)(
         input clk, rst,
         input start_get_cmd,
 		input [15 : 0] command_in,
@@ -10,6 +10,7 @@ module get_command_FSM_3
 		output reg en_rd_cmd,
         output reg done_get_cmd,
 		output reg [log2(buffer_size)-1 : 0] rd_addr_command_updated,
+		output reg [log2(buffer_size)-1 : 0] rd_addr_data_updated,
         output reg [7 : 0] instr,
         output reg [2 : 0] arg1,
 		output reg [4 : 0] arg2,
@@ -21,8 +22,10 @@ module get_command_FSM_3
 		reg [4 : 0] next_arg2;
 		reg [1 : 0] next_error;
 		reg [log2(buffer_size)-1 : 0] next_rd_addr_command;
+		reg [log2(buffer_size)-1 : 0] next_rd_addr_data;
 
-    localparam STATE_START = 2'b00, STATE_GET_CMD = 2'b01, STATE_CHECK_ERR = 2'b10, STATE_END = 2'b11;
+    localparam STATE_START = 2'b00, STATE_GET_CMD = 2'b01, 
+				STATE_CHECK_ERR = 2'b10, STATE_END = 2'b11;
 
 	always @(posedge clk or negedge rst) begin
 		if (! rst) begin
@@ -54,7 +57,7 @@ module get_command_FSM_3
 			end
 
 			STATE_GET_CMD:
-				next_state <= STATE_CHECK_ERROR;
+				next_state <= STATE_CHECK_ERR;
 
 			STATE_CHECK_ERR:
 				next_state <= STATE_END;
@@ -174,4 +177,20 @@ module get_command_FSM_3
 
 		endcase
 	end
+
+	function integer log2;
+    input [31 : 0] value;
+     integer i;
+    begin
+          if(value==1)
+                log2=1;
+          else
+              begin
+              i = value - 1;
+              for (log2 = 0; i > 0; log2 = log2 + 1) begin
+                    i = i >> 1;
+              end
+              end
+    end
+    endfunction
 endmodule
